3db02322c3dc6b0c9e8833cbe0e6594bb67de19e
kamrul95 <kamrulislam.bd95@gmail.com>
2024-11-02 22:21:37 +0600

    added relation between user and bookmarks

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,7 +1,8 @@
-import { Injectable } from "@nestjs/common";
+import { ForbiddenException, Injectable } from "@nestjs/common";
 import { AuthDto } from "./dto/auth.dto";
 import * as argon  from "argon2";
 import { PrismaService } from "src/prisma/prisma.service";
+import { PrismaClientKnownRequestError } from "@prisma/client/runtime/library";
 
 @Injectable()
 export class AuthService {
@@ -11,14 +12,24 @@ async signUp(AuthDto: AuthDto) {
     const hash = await argon.hash(AuthDto.password);
     console.log(hash);
     // create user
-    const user = await this.prisma.user.create({
-        data: {
-            email: AuthDto.email,
-            hash,
+    try {
+        const user = await this.prisma.user.create({
+            data: {
+                email: AuthDto.email,
+                hash,
+            }
+        });
+        delete user.hash;
+        return user;
+    } catch (error) {
+        if(error instanceof PrismaClientKnownRequestError) {
+            if(error.code === 'P2002') {
+                throw new ForbiddenException('Credentials already taken');
+            }
+        } else {
+            throw error;
         }
-    });
-    delete user.hash;
-    return user;
+    }
 }
 signIn() {
     return {msg: 'I am signed in'};
